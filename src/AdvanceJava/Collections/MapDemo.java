b53f6dfc172e9484461b7fa813b787fcf3a218aa
marzie <niloo.ashk@gmail.com>
2023-08-15 12:50:20 -0700

    Advance Java: finish Collections

--- src/AdvanceJava/Collections/MapDemo.java
+++ src/AdvanceJava/Collections/MapDemo.java
@@ -8,24 +8,34 @@ public class MapDemo {
     var c1 = new Customer("a", "e1");
     var c2 = new Customer("b", "e2");
 
+//    Java : Map
+//    Python , C# : Dictionary
+//    Java Scripts : Objects
     Map<String, Customer> map = new HashMap<>();
     map.put(c1.getEmail(), c1);
     map.put(c2.getEmail(), c2);
 
     var exists = map.containsKey("e1");
+    System.out.println(exists);
 
     var unknown = new Customer("Unknown", "");
     var customer = map.get("e1");
-    customer = map.getOrDefault("e1", unknown);
+    System.out.println(customer);
+    customer = map.getOrDefault("e10", unknown);
+    System.out.println(customer);
 
     map.replace("e1", new Customer("a++", "e1"));
+    System.out.println(map);
 
+//    this returns a list of keys
     for (var key : map.keySet())
       System.out.println(key);
 
+//    this returns a list of values
     for (var value : map.values())
       System.out.println(value);
 
+//    this returns the entry each key and value
     for (var entry : map.entrySet())
       System.out.println(entry);
   }
---
+++ src/OOP/Resizable.java
@@ -0,0 +1,5 @@
+package OOP;
+
+public interface Resizable {
+    void resize(int size);
+}
